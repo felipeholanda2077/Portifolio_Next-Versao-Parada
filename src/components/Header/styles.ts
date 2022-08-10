9cc5b2fa4f281964dc2f7e3aa8e84074e552f3ef
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2022-08-10 17:00:44 -0300

    Criação Da Home Base

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import { lighten } from 'polished';
+import styled from 'styled-components';
+
+interface NavLinkProps {
+  isActive: boolean;
+}
+
+export const Container = styled.header`
+  width: 100%;
+  display: flex;
+  aling-items: center;
+  justify-content: center;
+  height: 4rem;
+  border-bottom: 1px solid ${({ theme }) => theme.backgroundLight};
+
+  ul {
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+  }
+`;
+
+export const NavLinkContainer = styled.li<NavLinkProps>`
+  a {
+    text-transform: uppercase;
+    color: ${props =>
+      props.isActive ? props.theme.primary : props.theme.textHighLight};
+    transition: 0.5s;
+
+    &:hover {
+       color: ${props =>
+         props.isActive
+           ? lighten(0.2, props.theme.primary)
+           : lighten(0.2, props.theme.textHighLight)}; 
+  }
+`;
